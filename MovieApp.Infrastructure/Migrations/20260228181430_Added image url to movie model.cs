243f8b3a2b28cb9f76d641d98ce1630f07e8da7c
adelrose <adelrose911@gmail.com>
2026-03-02 22:34:23 +0400

    .

---
+++ MovieApp.Infrastructure/Migrations/20260228181430_Added image url to movie model.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addedimageurltomoviemodel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Movies");
+        }
+    }
+}
